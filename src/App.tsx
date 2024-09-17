854fc99dbdc2f12f318b2e475ad5d44fbcb955a3
Audrey Yang <ayang.penn@gmail.com>
2024-09-16 22:04:03 -0500

    Switch to dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-import { Typography } from "@mui/material";
+import {
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
 import TaskList from "./components/TaskList";
 import Status from "./components/Status";
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 const App = () => {
   return (
-    <div className="App">
-      <Typography variant="h3" className="my-2">
-        Taskflow
-      </Typography>
-      <Status />
-      <TaskList />
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div className="App">
+        <Typography variant="h3" className="my-2">
+          Taskflow
+        </Typography>
+        <Status />
+        <TaskList />
+      </div>
+    </ThemeProvider>
   );
 };
 
